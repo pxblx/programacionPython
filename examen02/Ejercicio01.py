dca152889f43f95225ff5608c4f90f750b981f5d
pablo <pablomurilloavila@gmail.com>
2018-12-14 15:57:25 +0100

    Examen del primer trimestre (incompleto)

---
+++ examen02/Ejercicio01.py
@@ -0,0 +1,14 @@
+#
+# Ejercicio 1 de Python
+#
+# Pablo
+#
+
+# Pedir la cantidad de dinero
+dinero = float(input("Introduce la cantidad de dinero: "))
+
+# Pasar a céntimos
+dinero = dinero*100
+
+# Proceso
+
