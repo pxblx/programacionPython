2e854460c602f6a8a9b4d632a9564bea480a1731
pxblx <pablomurilloavila@gmail.com>
2019-05-23 04:07:08 +0200

    Examen del segundo trimestre (correcciones 1) (fixes #1, fixes #2)

--- examen03/ejercicio03/src/Cuadrado.py
+++ examen03/ejercicio03/src/Cuadrado.py
@@ -8,8 +8,29 @@ class Cuadrado(Rectangulo):
     def __init__(self, lado):
         super().__init__(lado, lado)
 
+    @property
+    def lado(self):
+        return self.alto
+
+    @lado.setter
+    def lado(self, lado):
+        self.alto = lado
+        self.ancho = lado
+
     def __eq__(self, other):
         if isinstance(other, Cuadrado):
-            if self.alto == other.alto:
+            if self.lado == other.lado:
+                return True
+        return False
+
+    def __gt__(self, other):
+        if isinstance(other, Cuadrado):
+            if self.lado > other.lado:
+                return True
+        return False
+
+    def __ge__(self, other):
+        if isinstance(other, Cuadrado):
+            if self.lado > other.lado or self.lado == other.lado:
                 return True
         return False
--- examen03/ejercicio03/src/Rectangulo.py
+++ examen03/ejercicio03/src/Rectangulo.py
@@ -4,10 +4,8 @@ Ejercicio 3 (Rectangulo)
 
 class Rectangulo(object):
     def __init__(self, ancho, alto):
-        Rectangulo.__comprobar_alto_ancho(ancho)
-        Rectangulo.__comprobar_alto_ancho(alto)
-        self.__ancho = ancho
-        self.__alto = alto
+        self.ancho = ancho
+        self.alto = alto
 
     @property
     def ancho(self):
@@ -25,16 +23,16 @@ class Rectangulo(object):
     @alto.setter
     def alto(self, alto):
         Rectangulo.__comprobar_alto_ancho(alto)
-        self.__ancho = alto
+        self.__alto = alto
 
     def __str__(self):
         resultado = ""
         for i in range(0, self.ancho):
             resultado += "[ ]"
         resultado += "\n"
-        for i in range(1, self.alto-1):
+        for i in range(1, self.alto - 1):
             resultado += "[ ]"
-            for espacios in range(1, self.ancho-1):
+            for espacios in range(1, self.ancho - 1):
                 resultado += "   "
             resultado += "[ ]\n"
         for i in range(0, self.ancho):
